dad4a9d923d1d2231b69b4f07069ddeccc8e2587
KeiKooLoc <43351979+KeiKooLoc@users.noreply.github.com>
2019-05-23 21:24:40 +0300

    Update user_home.py

--- modules/user/user_home.py
+++ modules/user/user_home.py
@@ -1,20 +1,190 @@
 # -*- coding: utf-8 -*-
 import logging
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, ConversationHandler, RegexHandler
+from telegram import ReplyKeyboardMarkup, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, ConversationHandler, RegexHandler, CallbackQueryHandler, MessageHandler, Filters
+from bson import ObjectId
 
 from db import calls_table, order_table, question_table
 from ..welcome import cancel, start
-from ..help_func import send_notification_to_admins
-from ..keyboards import yes_no_keyboard, user_home_keyboard
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+from ..help_func import Send
+from modules.helper_funcs.pagination import Pagination
+from modules.helper_funcs.templates import order_template
+
+from ..keyboards import yes_no_keyboard, user_home_keyboard, main_keyboard
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-USER_HOME, ORDERS, QUESTIONS, CALL, CANCEL_CALL, CONFIRM_CLEAR_ORDERS_LIST, CONFIRM_CLEAR_QUESTIONS_LIST = range(7)
+# USER_HOME, ORDERSs, QUESTIONS, CALL, CANCEL_CALL, CONFIRM_CLEAR_ORDERS_LIST, CONFIRM_CLEAR_QUESTIONS_LIST = range(7)
+
+START, ORDERS, CONFIRM_DELETE_ORDER, CALLS = range(4)
 
 
+def delete(bot, user_data, chat_id):
+    if 'to_delete' in user_data:
+        for msg_id in user_data['to_delete']:
+            bot.delete_message(chat_id, msg_id)
+        user_data['to_delete'] = list()
+    else:
+        user_data['to_delete'] = list()
+
+
+# TODO: remember last page when back to orders
+#       trash
+class UserHome(object):
+    def __init__(self):
+        self.start_keyboard = ReplyKeyboardMarkup([['Заказы'], ['Вопросы'], ['Заявка на перезвон'], ['Назад']])
+        self.orders_keyboard = ReplyKeyboardMarkup([['Показать все'], ['Показать оплаченые'], ['Показать не оплаченые'], ['Назад']])
+        self.start_message = '{}, добро пожаловать в Личный Кабинет'
+
+    def start(self, bot, update, user_data):
+        delete(bot, user_data, update.effective_chat.id)
+        bot.delete_message(update.effective_chat.id, update.message.message_id)
+
+        user_data['to_delete'].append(
+            update.message.reply_text(self.start_message.format(update.effective_user.full_name),
+                                      reply_markup=self.start_keyboard).message_id)
+        return START
+
+    # delete messages
+    def orders(self, bot, update, user_data):
+        delete(bot, user_data, update.effective_chat.id)
+        bot.send_chat_action(update.effective_chat.id, action=ChatAction.TYPING)
+
+        if order_table.find({'user_id': update.effective_user.id}).count() == 0:
+            user_data['to_delete'].append(
+                update.message.reply_text('Нет заказов', reply_markup=self.start_keyboard))
+            return START
+        # maybe don't show buttons that got no effect
+        if update.message:
+            bot.delete_message(update.effective_chat.id, update.effective_message.message_id)
+            page = 1
+            if update.message.text == 'Показать оплаченые':
+                user_data['filter'] = True
+                self.orders_keyboard = ReplyKeyboardMarkup([['Показать все'], ['Показать не оплаченые'], ['Назад']])
+
+            elif update.message.text == 'Показать не оплаченые':
+                user_data['filter'] = False
+                self.orders_keyboard = ReplyKeyboardMarkup([['Показать все'], ['Показать оплаченые'], ['Назад']])
+
+            elif update.message.text == 'Показать все' or update.message.text == 'Заказы':
+                user_data['filter'] = 'all'
+                self.orders_keyboard = ReplyKeyboardMarkup([['Показать оплаченые'], ['Показать не оплаченые'], ['Назад']])
+        else:
+            # TODO: correct it. ValueError when back from finish_delete_order
+            #       remember last page when back to orders
+            try:
+                page = int(update.callback_query.data)
+            except ValueError:
+                page = 1
+        data, pages_keyboard, total_pages = \
+            Pagination(order_table, status=user_data['filter'], user_id=update.effective_user.id).get(page=page)
+
+        if len(data) == 0:
+            user_data['to_delete'].append(
+                update.message.reply_text('Нет Заказов', reply_markup=self.start_keyboard)
+            )
+            return START
+
+        Send(bot, data, update.effective_chat.id, user_data).send_orders_to_user(data)
+
+        if total_pages > 1:
+            user_data['to_delete'].append(
+                bot.send_message(update.effective_chat.id, 'Current page: {}'.format(str(page)),
+                                 reply_markup=pages_keyboard).message_id
+            )
+        user_data['to_delete'].append(
+            bot.send_message(update.effective_chat.id, '\u2060', reply_markup=self.orders_keyboard).message_id
+        )
+        return ORDERS
+
+    def confirm_delete_order(self, bot, update, user_data):
+        delete(bot, user_data, update.effective_chat.id)
+        # bot.delete_message(update.effective_chat.id, update.effective_message.message_id)
+
+        order = order_table.find_one({"_id": ObjectId(update.callback_query.data.split('/')[1])})
+        if order:
+            user_data['to_delete'].append(
+                bot.send_message(update.effective_chat.id,
+                                 'Все удалённые записи перемещаются в корзину из которой их можно восстановить. '
+                                 '\nУдалить запись?', reply_markup=ReplyKeyboardMarkup([['Назад']])).message_id
+            )
+            # use callback data or user_data?
+            user_data['to_delete'].append(
+                bot.send_message(update.effective_chat.id, order_template(order, comment=False),
+                                 reply_markup=InlineKeyboardMarkup(
+                                 [[InlineKeyboardButton('Удалить', callback_data='id/{}'.format(order['_id']))]])).message_id
+            )
+
+        else:
+            # TODO correct
+            pass
+        return CONFIRM_DELETE_ORDER
+
+    def finish_delete_order(self, bot, update, user_data):
+        move_to_trash = \
+            order_table.update_one({'_id': ObjectId(update.callback_query.data.split('/')[1])}, {'$set': {'in_user_trash': True}})
+        if move_to_trash.raw_result['n']:
+            user_data['to_delete'].append(
+                bot.send_message(update.effective_chat.id, 'Заказ удалён успешно').message_id
+            )
+            bot.answer_callback_query(update.callback_query.id, 'Заказ перенесён в корзину')
+            return self.orders(bot, update, user_data)
+        else:
+            # TODO correct
+            user_data['to_delete'].append(
+                bot.send_message(chat_id=update.effective_chat.id, text='не удалось удалить заказ, видимо он уже удалён',
+                                 reply_markup=self.start_keyboard).message_id)
+        return START
+
+    def pay(self, bot, update, user_data):
+        pass
+
+    def back(self, bot, update):
+        bot.delete_message(update.effective_chat.id, update.message.message_id)
+        update.message.reply_text('Sweet Jumble первое и самое крупное агентство персональных стилистов-шопперов в '
+                                  'Украине', reply_markup=ReplyKeyboardMarkup(main_keyboard))
+        return ConversationHandler.END
+
+
+user_home_conv = ConversationHandler(
+    entry_points=[RegexHandler('^Личный кабинет$', UserHome().start, pass_user_data=True),
+                  CommandHandler('home', UserHome().start, pass_user_data=True)],
+
+    states={
+        START: [RegexHandler('^Заказы$', UserHome().orders, pass_user_data=True),
+                # RegexHandler('^Вопросы$', questions),
+                # RegexHandler('^Заявка на перезвон$', user_call, pass_user_data=True),
+                RegexHandler('^Назад$', UserHome().back),
+                ],
+
+        ORDERS: [CallbackQueryHandler(UserHome().orders, pattern='^[0-9]+$', pass_user_data=True),
+                 CallbackQueryHandler(UserHome().confirm_delete_order, pattern=r'delete', pass_user_data=True),
+                 # CallbackQueryHandler(UserHome().pay, pattern=r'pay', pass_user_data=True),
+                 RegexHandler('^Назад$', UserHome().start, pass_user_data=True),
+                 MessageHandler(Filters.text, UserHome().orders, pass_user_data=True)  # TODO: change this line
+                                                                                       #          (order_status_filter)
+        ],
+
+        CONFIRM_DELETE_ORDER: [CallbackQueryHandler(UserHome().finish_delete_order, pattern=r'id', pass_user_data=True),
+                               RegexHandler('^Назад$', UserHome().orders, pass_user_data=True)]
+
+        # CALLS: [RegexHandler('^Удалить заявку$', cancel_call, pass_user_data=True),
+        #        RegexHandler('Назад', user_home)],
+
+        # CANCEL_CALL: [RegexHandler('^Назад$', user_call, pass_user_data=True),
+        #               RegexHandler('^Да$', cancel_call_success, pass_user_data=True)],
+
+        # QUESTIONS: [  # RegexHandler('^Очистить список$', confirm_clear_question_list),
+        #             RegexHandler('^Назад$', user_home)],
+
+    },
+    fallbacks=[CommandHandler('cancel', cancel)],
+    allow_reentry=True
+)
+
+"""
 def user_home(bot, update):
     update.message.reply_text('Это ваш личный кабинет где вы можете просмотреть овтеты на ваши вопросы или ваши заказы',
                               reply_markup=ReplyKeyboardMarkup(user_home_keyboard, one_time_keyboard=True))
@@ -52,16 +222,14 @@ def questions(bot, update):
 
 
 def user_call(bot, update, user_data):
-    call_me_data = calls_table.find_one({'username': update.message.chat.username, 'have_called': 0})
+    call_me_data = calls_table.find_one({'user_id': update.effective_user.id, 'status': False})
     if not call_me_data:
         update.message.reply_text('У вас нет активных заявок на перезвон',
                                   reply_markup=ReplyKeyboardMarkup(user_home_keyboard, one_time_keyboard=True))
         return USER_HOME
 
     keyboard = [['Удалить заявку'], ['Назад']]
-    data_to_send = 'Мы вам наберём ' + str(call_me_data['date']['month']) + ':' + str(call_me_data['date']['day']) + \
-                   '\nВ ' + str(call_me_data['date']['hour']) + ':' + str(call_me_data['date']['min']) + \
-                   '\nНа номер: ' + call_me_data['number']
+    data_to_send = 'Мы вам наберём ' + call_template(call_me_data)
     user_data['data_to_send'] = data_to_send
     user_data['_id'] = call_me_data['_id']
     update.message.reply_text(data_to_send,
@@ -83,55 +251,27 @@ def cancel_call_success(bot, update, user_data):
     return USER_HOME
 
 
-"""
-def confirm_clear_order_list(bot, update):
-    keyboard = [['Да', 'Назад']]
-    update.message.reply_text(data['user']['orders'] + '      Удалить все записи?',
-                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
-    return CONFIRM_CLEAR_ORDERS_LIST
-
-
-def questions(bot, update):
-    keyboard = [['Очистить список'], ['Назад']]
-    # question_table.find({'username':})
-    update.message.reply_text(data['user']['questions'],
-                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
-    return QUESTIONS
-
-
-def confirm_clear_question_list(bot, update):
-    keyboard = [['Да', 'Назад']]
-    update.message.reply_text(data['user']['questions'] + '      Удалить все записи?',
-                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
-    return CONFIRM_CLEAR_QUESTIONS_LIST
-
-
-def clear_list_confirm_true(bot, update):
-    update.message.reply_text('Записи удалены',
-                              reply_markup=ReplyKeyboardMarkup(user_home_keyboard, one_time_keyboard=True))
-    return USER_HOME
-
-"""
-
 
 user_home_conv = ConversationHandler(
-    entry_points=[RegexHandler('^Личный кабинет$', user_home),
+    entry_points=[RegexHandler('^Личный кабинет$', UserHome().start),
                   CommandHandler('home', user_home)],
 
     states={
-        USER_HOME: [RegexHandler('^Заказы$', orders),
+        USER_HOME: [RegexHandler('^Заказы$', UserHome().orders, pass_user_data=True),
                     RegexHandler('^Вопросы$', questions),
                     RegexHandler('^Заявка на перезвон$', user_call, pass_user_data=True),
                     RegexHandler('^Назад$', start)],
 
-        CALL: [RegexHandler('^Удалить заявку$', cancel_call, pass_user_data=True),
+        ORDERS: [CallbackQueryHandler(UserHome().confirm_delete, pass_user_data=True)],
+
+        CONFIRM_DELETE_ORDER: [],
+
+        CALLS: [RegexHandler('^Удалить заявку$', cancel_call, pass_user_data=True),
                RegexHandler('Назад', user_home)],
 
         CANCEL_CALL: [RegexHandler('^Назад$', user_call, pass_user_data=True),
                       RegexHandler('^Да$', cancel_call_success, pass_user_data=True)],
 
-        ORDERS: [  # RegexHandler('^Очистить список$', confirm_clear_order_list),
-                 RegexHandler('^Назад$', user_home)],
 
         # CONFIRM_CLEAR_ORDERS_LIST: [RegexHandler('^Да$', clear_list_confirm_true),
         #                            RegexHandler('^Назад$', orders)],
@@ -145,3 +285,4 @@ user_home_conv = ConversationHandler(
     fallbacks=[CommandHandler('cancel', cancel)],
     allow_reentry=True
 )
+"""
